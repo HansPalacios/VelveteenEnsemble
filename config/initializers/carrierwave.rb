10832edce2e8280722dec6ac2dd63c85e005dfb0
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-07-15 17:33:39 -0400

    change carrierwave.rb so fog can be procompiled

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,21 +5,18 @@ require 'carrierwave/orm/activerecord'
 
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
+    config.storage :file
+  else
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider               => 'AWS',                            # required
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],         # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],     # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
+      region: ENV['S3_REGION'],
+      endpoint: 'https://s3.amazonaws.com'    # required
     }
     config.fog_directory  = ENV['BUCKET']                 # required
-    config.fog_public     = true                                   # optional, defaults to true
-    config.root = Rails.root.join('tmp')
-    config.cache_dir = 'files'
-    config.permissions = 0777
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
-else
-  config.storage :file
-    config.enable_processing = false if Rails.env.test?
+    config.fog_public     = false  
+    config.storage :fog                               # optional, defaults to true
   end
 end
