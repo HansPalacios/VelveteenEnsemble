21382fd452cd63adbcd02849f55a3293b0b0e082
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-09-09 14:54:08 -0400

    js for hamburger menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,13 +2,25 @@
 
 ( function($) {
 $(document).ready(function(){	
- 	var $elements = $('.loopOne, .loopTwo, .loopThree, .loopFour');
+ 	var elements = $('.loopOne, .loopTwo, .loopThree, .loopFour');
+ 	var hamburger = $('.dropdown');
+ 	var dropdown = $('.dropdown-content');
 
 	function loop(index) {
-    $elements.eq(index).fadeIn(600).delay(4500).fadeOut(600, function() { 
+    elements.eq(index).fadeIn(600).delay(4500).fadeOut(600, function() { 
       loop((index + 1) % $elements.length);
     });
 	}
+
+	hambuger.click(function() {
+    if (dropdown.css.('display') === 'none') {
+      dropdown.show();
+      dropdown.css('display', 'block');
+    } else if (dropdown.css.('display') === 'block') {
+      dropdown.hide();
+      dropdown.css('display', 'none');
+    }
+  });
 
 	loop(0); 
 
