c482dba873bb9ef961f93739ce21b6ffc5488f11
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-09-19 12:39:53 -0400

    contact form dropdown shows input when other is selected

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMailer < ApplicationMailer
  def contactpage
     # @user = params[:user]
-    # @url  = 'http://example.com/login'
     # mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
