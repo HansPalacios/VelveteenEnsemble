87a4f66e87c7087b2fc5f4c8bd5d43ffd12e44ef
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-09-19 19:14:05 -0400

    please work type

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	var other = $('.other');
 
 	$('.type').change(function(){   
-		if(type.val() == 5)   
+		if(type.val() == "Other")   
 		  other.show(); 
 		else 
 		  other.hide(); 
