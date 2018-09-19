45e4199170fa21936df447f449bf0df67a7d3455
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-09-19 13:14:16 -0400

    margin top messagebox

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -1,45 +1,13 @@
 // hide dropdown when clicking on other option to fill out input
-// $( "#type_other_input" ).keypress(function() {
-//     $('#type_dropdown').removeClass();
-// });
 
-// $(document).ready(function() {
+$(document).ready(function() {   
+	var type = $('.type');
+	var other = $('.other');
 
-// 	var dropdown = $(".type_dropdown")
-// 	var input = $(".type_other_input")
-
-// 	dropdown.change(function(){   
-
-//   	if(dropdown.val() == 'Other') {
-//     	input.show();
-//   	}
-//     else {
-//     	input.hide();
-//     }
-// });
-	$(document).ready(function() {   
-$('.type').change(function(){   
-if($('.type').val() == "Other")   
-   {   
-   $('.other').show();    
-   }   
-else 
-   {   
-   $('.other').hide();      
-   }   
+	$('.type').change(function(){   
+		if(type.val() == "Other")   
+		  other.show(); 
+		else 
+		  other.hide(); 
+	});   
 });   
-});   
-// $(document).ready(function() {
-// $('#type_dropdown').change(function(){   
-// if($('#type_dropdown').val() === 'Other')   
-//    {   
-//    $('#type_other_input').show();    
-//    }   
-// else 
-//    {   
-//    $('#type_other_input').hide();      
-//    }   
-// });  
-// });
-
-  	
