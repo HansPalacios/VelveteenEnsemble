f66552f8f16cd9aeaa99e1c62d351af3fcd3d5cd
Hans-Sebastian <hanssebastian.p@gmail.com>
2018-09-19 15:01:22 -0400

    add attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,13 @@
 class Contact < MailForm::Base
   attribute :name, :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  # attribute :phone,   :validate => true
-  # attribute :eventdate,   :validate => true
-  # attribute :eventlocation,   :validate => true
-  # attribute :type,   :validate => true
-  # attribute :length,   :validate => true
+  attribute :phone,   :validate => true
+  attribute :eventdate,   :validate => true
+  attribute :eventlocation,   :validate => true
+  attribute :type,   :validate => true
+  attribute :length,   :validate => true
   attribute :message,   :validate => true
-  # attribute :nickname,  :captcha  => true
+  attribute :nickname,  :captcha  => true
   def headers
     {
       :subject => "Contact Form",
