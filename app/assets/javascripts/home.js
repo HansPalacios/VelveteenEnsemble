15d107e13437826cd69c34aa120a07dc24d45650
Hans-Sebastian <hanssebastian.p@gmail.com>
2019-10-02 21:52:00 -0400

    remove email popup and add widget

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 
 ( function($) {
 $(document).ready(function(){	
- 	var elements = $('.loopOne, .loopTwo, .loopThree, .loopFour');
+ 	var elements = $('.loopOne, .loopTwo, .loopThree, .loopFour, .loopFive');
  	var hamburger = $('.dropdown');
  	var dropdown = $('.dropdown-content');
 
