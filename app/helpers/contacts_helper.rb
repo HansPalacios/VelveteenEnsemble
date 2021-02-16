54ab13e6c508efb3d3f5ae9e437603d0479c51bd
Hans-Sebastian <hanssebastian.p@gmail.com>
2021-02-16 17:03:19 -0500

    “edit“

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -12,6 +12,5 @@ sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
-puts response.parsed_body
 puts response.headers
 end
