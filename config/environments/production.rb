c03d8262d6abd944b51ba58e76a20054592ac3b6
Hans-Sebastian <hanssebastian.p@gmail.com>
2021-02-17 21:28:33 -0500

    add :from to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
+  :from  => 'marcel@velveteenensemble.com',
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
