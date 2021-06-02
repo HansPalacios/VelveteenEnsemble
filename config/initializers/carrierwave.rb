1f7f0f1f0826234fbe5ce1d7cb773433a256bed5
Hans-Sebastian <hanssebastian.p@gmail.com>
2021-06-01 22:01:21 -0400

    oh good lets redo all the code

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,22 +11,44 @@ require 'carrierwave/orm/activerecord'
 #   }
 #   config.fog_directory  = ENV['BUCKET']
 # end
-CarrierWave.configure do |config|
+#CarrierWave.configure do |config|
   # if Rails.env.production?
     # config.storage/ :aws
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      :provider               => 'AWS',                            # required
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],         # required
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
-      region: 'us-east-1'
+   # config.fog_provider = 'fog/aws'
+   # config.fog_credentials = {
+   #   :provider               => 'AWS',                            # required
+    #  :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],         # required
+    #  :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
+   #   region: 'us-east-1'
       # endpoint: 'https://s3.amazonaws.com'    # required
-    }
-    config.fog_directory  = ENV['BUCKET']                 # required
-    config.fog_public     = true
+   # }
+   # config.fog_directory  = ENV['BUCKET']                 # required
+   # config.fog_public     = true
     # else
     # config.storage :file
     # config.enable_processing = false if Rails.env.test?
     #optional, defaults to true
   # end
+#end
+
+
+CarrierWave.configure do |config|
+  if Rails.env.test?
+    config.storage :file
+    config.asset_host = 'http://localhost:3000'
+  else
+    config.storage = :fog
+    config.fog_use_ssl_for_aws = true
+    config.fog_directory  = ENV['FOG_DIRECTORY']
+    config.fog_public     = true
+    config.fog_attributes = { 'Cache-Control': 'max-age=315576000' }
+    config.asset_host = 'https://s3.amazonaws.com/website'
+
+    config.fog_credentials = {
+      provider:               ENV['FOG_PROVIDER'],
+      aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+      path_style:             ENV['FOG_PATH_STYLE']
+    }
+  end
 end
