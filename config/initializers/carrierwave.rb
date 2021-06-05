6a008a488b91a1f9bdd61d3c4e4edf58cb3bb7f1
Hans-Sebastian <hanssebastian.p@gmail.com>
2021-06-05 16:55:08 -0400

    confused as always

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,7 +19,7 @@ CarrierWave.configure do |config|
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
     config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
+    config.root = lambda { "#{Rails.root}/public/uploads/#{Rails.env}" }
   else
     config.storage = :fog
   end
